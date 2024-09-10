c687419342fc6ca01ec1688ea04644fa74f442bb
rafael.mendes.negocios@gmail.com <rafael.mendes.negocios@gmail.com>
2024-09-09 21:09:48 -0300

    CONTADOR APP

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.contador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
+    int contador = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +20,14 @@ public class MainActivity extends AppCompatActivity{
         TextView myText = findViewById(R.id.textView);
         Button myButton = findViewById(R.id.button);
 
-        myText.setText("0");
-        myButton.setText("RANDOMIZAR");
+        myText.setText(Integer.toString(contador));
+        myButton.setText("AUMENTAR");
         myButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                myText.setText(Integer.toString((int)Math.random() * 100));
+                contador++;
+                myText.setText(Integer.toString(contador));
             }
         });
     }
