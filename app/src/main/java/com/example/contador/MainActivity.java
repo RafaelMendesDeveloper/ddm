f232c1d47b4bae009d48e1fbe3ee3043595eefed
Rafael Mendes <rafael.mendes.negocios@gmail.com>
2024-09-16 22:03:40 -0300

    navegação com dados

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.contador;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,19 +18,72 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.wtf("CICLO DE VIDA", "passou pelo OnCreate");
 
         Button buttonSave = findViewById(R.id.buttonSave);
+        Button buttonPage2 = findViewById(R.id.buttonPage2);
+        Button buttonPage3 = findViewById(R.id.buttonPage3);
+
         EditText notas = findViewById(R.id.editText1);
-        TextView notaVisualizada = findViewById(R.id.textViewNote);
 
         if(notas.getText() != null){
             buttonSave.setOnClickListener(v -> {
+
                 String noteText = notas.getText().toString();
 
-                notaVisualizada.setText(noteText);
-
+                String mensagem = noteText;
                 notas.setText("");
             });
         }
+
+        buttonPage2.setOnClickListener( v -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+
+            String mensagem = notas.getText().toString();
+
+            intent.putExtra("msg", mensagem);
+            startActivity(intent);
+        });
+
+        buttonPage3.setOnClickListener( v -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Ciclo de vida", "passou pelo onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Ciclo de vida", "passou pelo onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Ciclo de vida", "passou pelo onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Ciclo de vida", "passou pelo onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Ciclo de vida", "passou pelo onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Ciclo de vida", "passou pelo onDestroy");
     }
 }
---
+++ app/src/main/java/com/example/contador/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.contador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Bundle bundle = getIntent().getExtras();
+        String string = bundle.getString("msg");
+        TextView textView = findViewById(R.id.editTextId);
+        textView.setText(string);
+    }
+}
